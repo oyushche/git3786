ffaa508858d9fd26dc3fe434b8c687b5211334a1
oyushche <uhmateush@gmail.com>
2020-11-30 06:07:56 -0500

    M_3

--- src/Master.java
+++ src/Master.java
@@ -3,6 +3,7 @@ public class Master
     public static void main(String[] args)
     {
         System.out.println("1");
-        System.out.println("2");
+        System.out.println("2_2");
+        System.out.println("3");
     }
 }
